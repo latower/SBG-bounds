3cd30f72630e07474cd1538040a3586855279321
Anna L.D. Latour <a.l.d.latour@tudelft.nl>
2024-07-18 16:17:36 +0200

    Initial commit. Adding all input files, scripts, environment information, and README.

--- scripts/create_and_verify_SBG_cardinality_proofs.sh
+++ scripts/create_and_verify_SBG_cardinality_proofs.sh
@@ -1,11 +1,39 @@
 #!/bin/bash
 
+# Author:              Anna L.D. Latour
+# Creation date:       18 July 2024
+# Maintainer:          Anna L.D. Latour
+# Contact:             a.l.d.latour@tudelft.nl
+# File:                create_and_verify_SBG_cardinality_proofs.sh
+# Version:             0.0.1
+# Copyright:           (C) 2024, Anna L.D. Latour
+# License:             MIT
+
+# Copyright (C) 2024 Anna L.D. Latour
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
 while getopts r:v: flag
 do
     case "${flag}" in
         r) ROUNDINGSAT_DIR=${OPTARG};;
         v) VERIPB_DIR=${OPTARG};;
-        # f) fullname=${OPTARG};;
     esac
 done
 
